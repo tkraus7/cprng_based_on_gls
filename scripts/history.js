ef194608eb9e27d80ef493d73472a53e9e5198a8
Kraus, Tomáš <krausto8@fel.cvut.cz>
2023-05-06 14:47:56 +0200

    bugfixes and new icon

--- scripts/history.js
+++ scripts/history.js
@@ -5,15 +5,16 @@ function putValues(fields, values) {
     }
 }
 
+/* Recovers form values based on the popped history state. */
 let defaultState = [['-10', '10', '28', '1', '-8/3'], ['1', '1', '1'], ['5000', '0.01', '100']]
 function recoverHistory(e) {
     let state = e.state == null ? defaultState : e.state
-    // console.log(state)
 
     putValues(params, state[0])
     putValues(init_points, state[1])
     putValues(len_step_off, state[2])
     updateEq()
+    check_seq_p()
 }
 
 window.addEventListener("popstate", recoverHistory)
